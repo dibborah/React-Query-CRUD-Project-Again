0253a9777f8b3183ee0923003a7f238e56566e68
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-13 02:44:35 +0530

    Routes are created along with the neccessary pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { EditPost, Post, PostList } from "./pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Awesome Blog</h1>
+      <Routes>
+        <Route path="/" element={<PostList />}></Route>
+        <Route path="/post/:id" element={<Post />}></Route>
+        <Route path="/post/:id/edit" element={<EditPost />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import PostList from "./PostList";
+import Post from "./Post";
+import EditPost from "./EditPost";
+
+export { PostList, Post, EditPost };
